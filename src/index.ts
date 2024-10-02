4a3ff47664bc029965a80e1806cf2f91cbf6e14c
Benjamin Smith <contact@bensmith.cloud>
2024-10-02 19:38:20 +0100

    Integrated typescript

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,74 @@
-import { Project, IProject, ProjectStatus,UserRole } from "./class/Project";
+import { IProject, ProjectStatus, UserRole } from "./classes/Project"
+import { ProjectsManager } from "./classes/ProjectsManager"
 
 function showModal(id: string) {
-    const modal = document.getElementById(id)
-    if (modal && modal instanceof HTMLDialogElement) {
-      modal.showModal()
-    } else {
-      console.warn("The provided modal wasn't found. ID: ", id)
+  const modal = document.getElementById(id)
+  if (modal && modal instanceof HTMLDialogElement) {
+    modal.showModal()
+  } else {
+    console.warn("The provided modal wasn't found. ID: ", id)
+  }
+}
+
+function closeModal(id: string) {
+  const modal = document.getElementById(id)
+  if (modal && modal instanceof HTMLDialogElement) {
+    modal.close()
+  } else {
+    console.warn("The provided modal wasn't found. ID: ", id)
+  }
+}
+
+function toggleModal() {
+    const modal = document.getElementById("new-project-modal") as HTMLDialogElement;
+    if (modal) {
+      if (modal.open) {
+        modal.close();
+      } else {
+        modal.showModal();
+      }
     }
   }
 
-// This document object is provided by the browser, and its main purpose is to help us interact with the DOM.
-const newProjectButton = document.getElementById('new-project-button');
+const projectsListUI = document.getElementById("projects-list") as HTMLElement
+const projectsManager = new ProjectsManager(projectsListUI)
 
-if (newProjectButton) {
-    newProjectButton.addEventListener("click", () => {
-        showModal("new-project-modal")
-    });
+// This document object is provided by the browser, and its main purpose is to help us interact with the DOM.
+const newProjectBtn = document.getElementById("new-project-btn")
+if (newProjectBtn) {
+  newProjectBtn.addEventListener("click", () => {showModal("new-project-modal")})
 } else {
-    console.warn("New project button was not found");
+  console.warn("New projects button was not found")
 }
 
 const projectForm = document.getElementById("new-project-form")
-
 if (projectForm && projectForm instanceof HTMLFormElement) {
-    projectForm.addEventListener("submit", (e) => {
-        e.preventDefault()
-        const formData = new FormData(projectForm)
-        const projectData: IProject= {
-            name: formData.get("name") as string,
-            description: formData.get("description") as string,
-            status: formData.get("status") as ProjectStatus,
-            userRole: formData.get("userRole") as UserRole,
-            finishDate: new Date(formData.get("finishDate") as string),
-        }
-
-        const project = new Project(projectData)
-        console.log("New Project Submitted: ", project);
-    });
+  projectForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const formData = new FormData(projectForm)
+    const projectData: IProject = {
+      name: formData.get("name") as string,
+      description: formData.get("description") as string,
+      status: formData.get("status") as ProjectStatus,
+      userRole: formData.get("userRole") as UserRole,
+      finishDate: new Date(formData.get("finishDate") as string)
+    }
+    try {
+      const project = projectsManager.newProject(projectData)
+      projectForm.reset()
+      closeModal("new-project-modal")
+    } catch (err) {
+      alert(err)
+    }
+  })
 } else {
-    console.warn("The project form was not found. Check the ID!");
+  console.warn(
+    "The project form was not found. Check the ID!"
+    );  
 }
 
-
-// const newProjectButton = document.getElementById('new-project-button');
-// const newProjectModal = document.getElementById('new-project-modal');
-// const cancelButton = document.getElementById('cancel-button');
-// const projectDetailsPage = document.getElementById('project-details');
-// const projectsPage = document.getElementById('projects-page');
-
-// newProjectButton.onclick = () => {
-//   newProjectModal.showModal();
-// };
-
-// cancelButton.onclick = () => {
-//   newProjectModal.close();
-// };
-
-// function showProjectDetails() {
-//   projectsPage.style.display = 'none';
-//   projectDetailsPage.style.display = 'flex';
-// }
+// Close modal when cancel button is clicked
+document.querySelector("button[type='button']")?.addEventListener('click', () => {
+    toggleModal();
+  });
+  
