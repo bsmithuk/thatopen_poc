53f0a456c72d09808f90c6ffa2ca6ef64233fecf
Benjamin Smith <contact@bensmith.cloud>
2024-10-06 10:51:30 +0100

    Added random colour function and substitution of this.name to two characters on Project list page

--- src/classes/Project.ts
+++ src/classes/Project.ts
@@ -1,46 +1,55 @@
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 
-export type ProjectStatus = "pending" | "active" | "finished"
-export type UserRole = "architect" | "engineer" | "developer"
+function getRandomColour() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+export type ProjectStatus = "pending" | "active" | "finished";
+export type UserRole = "architect" | "engineer" | "developer";
 
 export interface IProject {
-  name: string
-	description: string
-	status: ProjectStatus
-	userRole: UserRole
-	finishDate: Date
+  name: string;
+  description: string;
+  status: ProjectStatus;
+  userRole: UserRole;
+  finishDate: Date;
 }
 
 export class Project implements IProject {
-	//To satisfy IProject
-  name: string
-	description: string
-	status: "pending" | "active" | "finished"
-	userRole: "architect" | "engineer" | "developer"
-  finishDate: Date
-  
+  //To satisfy IProject
+  name: string;
+  description: string;
+  status: "pending" | "active" | "finished";
+  userRole: "architect" | "engineer" | "developer";
+  finishDate: Date;
   //Class internals
-  ui: HTMLDivElement
-  cost: number = 0
-  progress: number = 0
-  id: string
-
+  ui: HTMLDivElement;
+  cost: number = 0;
+  progress: number = 0;
+  id: string;
   constructor(data: IProject) {
     //Project data definition
-    this.id = uuidv4()
+    this.id = uuidv4();
     for (const key in data) {
-      this[key] = data[key]
+      this[key] = data[key];
     }
-    this.setUI()
+    this.setUI();
   }
-
+  //Pull UI into 'Project'
   setUI() {
-    if (this.ui) {return}
-    this.ui = document.createElement("div")
-    this.ui.className = "project-card"
+    if (this.ui) {
+      return;
+    }
+    this.ui = document.createElement("div");
+    this.ui.className = "project-card";
     this.ui.innerHTML = `
     <div class="card-header">
-      <p style="background-color: #ca8134; padding: 10px; border-radius: 8px; aspect-ratio: 1;">HC</p>
+      <p style="background-color: ${getRandomColour()}; padding: 10px; border-radius: 8px; aspect-ratio: 1;">${this.name.slice(0, 2).toUpperCase()}</p>
       <div>
         <h5>${this.name}</h5>
         <p>${this.description}</p>
@@ -63,7 +72,6 @@ export class Project implements IProject {
         <p style="color: #969696;">Estimated Progress</p>
         <p>${this.progress * 100}%</p>
       </div>
-    </div>`
+    </div>`;
   }
 }
-
